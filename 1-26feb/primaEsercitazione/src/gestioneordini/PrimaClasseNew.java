57991d4f97aeda152f2c34fc02a0417211efffa4
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-02-26 13:23:48 +0000

    abbiamo imparato Scanner e import utils

--- 1-26feb/primaEsercitazione/src/gestioneordini/PrimaClasseNew.java
+++ 1-26feb/primaEsercitazione/src/gestioneordini/PrimaClasseNew.java
@@ -1,7 +1,12 @@
 package gestioneordini;
+import java.util.Scanner;
 
 public class PrimaClasseNew {
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Benvenuti al corso1");
+		String nome=sc.nextLine();
+		System.out.println("Ciao il tuo è " + nome);
+		sc.close();
 	};
 }
