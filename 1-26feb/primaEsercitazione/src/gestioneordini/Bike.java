cc572e2dfaff4d578ed7caf13dc6c0f9bdce3c81
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-02-26 13:46:20 +0000

    classe Bike e imparato il debugger

---
+++ 1-26feb/primaEsercitazione/src/gestioneordini/Bike.java
@@ -0,0 +1,9 @@
+package gestioneordini;
+
+public class Bike {
+	private int gear;
+	private int cadence;
+	private int boh;
+	
+	Bike() {}
+}
--- 1-26feb/primaEsercitazione/src/gestioneordini/PrimaClasseNew.java
+++ 1-26feb/primaEsercitazione/src/gestioneordini/PrimaClasseNew.java
@@ -2,11 +2,16 @@ package gestioneordini;
 import java.util.Scanner;
 
 public class PrimaClasseNew {
+//	un solo main
+//	static è important
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Benvenuti al corso1");
 		String nome=sc.nextLine();
 		System.out.println("Ciao il tuo è " + nome);
 		sc.close();
+		
+		Bike bike1=new Bike();
+		Bike bike2=new Bike();
 	};
 }
