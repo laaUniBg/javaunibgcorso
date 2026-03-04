d0f5f72f39f436e190f763f84026c23c12efe36b
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-03-04 11:49:40 +0000

    salvato coddice prima della pausa, non ho ricopiato tutto, in più ho ordinato in cartelle

---
+++ lezioni/1-4marzo/Frazione/src/Frazione/Client.java
@@ -0,0 +1,8 @@
+package Frazione;
+
+public class Client {
+	public static void main(String[] args) {
+		Frazione f=new Frazione(2,3);
+		f.stampa();
+	}
+}
---
+++ lezioni/1-4marzo/Frazione/src/Frazione/Frazione.java
@@ -0,0 +1,11 @@
+package Frazione;
+
+public class Frazione {
+	public static void main(String[] args) {
+		// non sono riuscito a ricopiare in tempo
+	}
+	
+	void stampa() {
+		
+	}
+}
