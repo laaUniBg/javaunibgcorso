76462e9a1988324d1a332a03529b19624e522f3b
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-03-04 11:56:04 +0000

    save

--- lezioni/1-4marzo/Frazione/src/Frazione/Client.java
+++ lezioni/1-4marzo/Frazione/src/Frazione/Client.java
@@ -3,6 +3,7 @@ package Frazione;
 public class Client {
 	public static void main(String[] args) {
 		Frazione f=new Frazione(2,3);
+		int a=0;
 		f.stampa();
 	}
 }
--- lezioni/1-4marzo/Frazione/src/Frazione/Frazione.java
+++ lezioni/1-4marzo/Frazione/src/Frazione/Frazione.java
@@ -4,6 +4,7 @@ public class Frazione {
 	public static void main(String[] args) {
 		// non sono riuscito a ricopiare in tempo
 	}
+	Frazione(int x, int y){};
 	
 	void stampa() {
 		
