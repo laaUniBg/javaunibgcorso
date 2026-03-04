20fc84d55d09cca90605712631667637a6d0f48d
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-03-04 11:59:12 +0000

    Imparato new Integer

--- lezioni/1-4marzo/Frazione/src/Frazione/Client.java
+++ lezioni/1-4marzo/Frazione/src/Frazione/Client.java
@@ -4,6 +4,7 @@ public class Client {
 	public static void main(String[] args) {
 		Frazione f=new Frazione(2,3);
 		int a=0;
+		Integer b=new Integer(0); // per ogni tipo primitivo cè un suo oggetto anche se int è piu efficiente
 		f.stampa();
 	}
 }
