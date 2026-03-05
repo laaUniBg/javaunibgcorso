e774ac3b4137b180b57e9ea7c266656f26f53ead
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-03-05 13:12:52 +0000

    save

--- esercitazioni/5marzo/LettoreMP3/src/esercitazione2/LettoreMP3.java
+++ esercitazioni/5marzo/LettoreMP3/src/esercitazione2/LettoreMP3.java
@@ -1,8 +1,8 @@
 package esercitazione2;
 
 public class LettoreMP3 {
-	//sempre private poi solo public solo quando è necessario
-	private int numeroSerie;
+	// final significa che 'numeroSerie' posso inizializziarlo e basta una sola volta
+	private final int numeroSerie;
 	
 	public LettoreMP3(int numeroSerie) {
 //		this.numeroSerie=numeroSerie;
@@ -10,14 +10,14 @@ public class LettoreMP3 {
 	}
 	
 	// i setter sono più critici dei getter
-	public void setNumeroSerie(int numeroSerie) {
-		this.numeroSerie=numeroSerie;
-	}
+//	public void setNumeroSerie(int numeroSerie) {
+//		this.numeroSerie=numeroSerie;
+//	}
 	
 	public int getNumeroSerie() {
 		return this.numeroSerie;
 	}
 	
 	//oggetti mutabili dove lo stato puo cambiare
-	//oggetti immutabili dove lo stato non puo cambiare.
+	//oggetti immutabili dove lo stato non puo cambiare. -> uso private o non uso i setter
 }
