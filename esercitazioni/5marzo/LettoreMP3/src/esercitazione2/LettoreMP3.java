208c319d17fc4b85b5334241bb9e82cf0037ba72
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-03-05 13:28:23 +0000

    metodo sbagliato per farci capire. il metodo sbagliato per salvare fra diversi oggetti i dati

--- esercitazioni/5marzo/LettoreMP3/src/esercitazione2/LettoreMP3.java
+++ esercitazioni/5marzo/LettoreMP3/src/esercitazione2/LettoreMP3.java
@@ -2,12 +2,20 @@ package esercitazione2;
 
 public class LettoreMP3 {
 	private int numeroSerie;
+//	private int[] numeroSerieUsati=new int[10];
+//	private int numerodiLettoriMP3;
 	
 	public LettoreMP3(int numeroSerie) {
 		if(numeroSerie <0) {
 			throw new IllegalArgumentException("Non puoi " + "creare un " + "MP3 con seriale");
 		}
 		this.numeroSerie=numeroSerie;
+		
+		// verificare che non sia dentro numeriSerieUsati
+		
+//		numeriSerieUsati[numerodiLettoriMP3]=numeroSerie;
+//		numerodiLettoriMP3++;
+//		this.numeroSerie=numeroSerie;
 	}
 	
 	public void setNumeroSerie(int numeroSerie) {
