953a9020fbb8cb6a980006b492132b1b0ef20152
laaUnibgTab <a.laaouatni@studenti.unibg.it>
2026-03-05 13:52:40 +0000

    prima prova codice da solo setDiInteri

---
+++ esercitazioni/5marzo/esercizioDaSolo/SetDiInteri/src/esercitazione2_1/Main.java
@@ -0,0 +1,9 @@
+package esercitazione2_1;
+
+public class Main {
+	public static void main(String[] args) {
+		SetInteri thisSet = new SetInteri();
+		thisSet.myPush(10);
+		thisSet.myPush(20);
+	}
+};
---
+++ esercitazioni/5marzo/esercizioDaSolo/SetDiInteri/src/esercitazione2_1/SetInteri.java
@@ -0,0 +1,15 @@
+package esercitazione2_1;
+
+public class SetInteri {
+	private int numArray[]=new int[10];
+	private int lastIndex = 0;
+	
+	public SetInteri() {
+		
+	}
+	
+	public void myPush(int n) {
+		this.numArray[lastIndex] = n;
+		lastIndex++;
+	}
+}
